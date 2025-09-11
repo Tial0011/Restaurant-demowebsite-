0172412df881e3411e39241d75f13d43eb25f92f
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-11 19:35:41 +0100

    i used a div instead of an input box for the qty

---
+++ menu.js
@@ -0,0 +1,15 @@
+const foods = document.querySelectorAll(".afood");
+foods.forEach((food) => {
+  const minusBtn = food.querySelector(".minus");
+  const plusBtn = food.querySelector(".plus");
+  const qtyDiv = food.querySelector(".quantity");
+  let qty = parseInt(qtyDiv.textContent);
+  plusBtn.addEventListener("click", () => {
+    qty++;
+    qtyDiv.textContent = qty;
+  });
+  minusBtn.addEventListener("click", () => {
+    if (qty > 0) qty--;
+    qtyDiv.textContent = qty;
+  });
+});
