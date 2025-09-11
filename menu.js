df39fd168b1576687ae106fc543ef55044c2af1f
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-11 19:50:54 +0100

    i gave add to cart btn addqty functionalty

--- menu.js
+++ menu.js
@@ -1,4 +1,5 @@
 const foods = document.querySelectorAll(".afood");
+const crt = document.querySelector(".crtbtn");
 foods.forEach((food) => {
   const minusBtn = food.querySelector(".minus");
   const plusBtn = food.querySelector(".plus");
@@ -10,6 +11,10 @@ foods.forEach((food) => {
   });
   minusBtn.addEventListener("click", () => {
     if (qty > 0) qty--;
+    qtyDiv.textContent = qty;
+  });
+  crt.addEventListener("click", () => {
+    qty++;
     qtyDiv.textContent = qty;
   });
 });
