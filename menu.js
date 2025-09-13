eb751fb225631d4655fa61ee5bb03c45186399b2
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2025-09-13 15:47:10 +0100

    i made the btns functional and adjusted the css

--- menu.js
+++ menu.js
@@ -1,20 +1,78 @@
-const foods = document.querySelectorAll(".afood");
+const card = document.querySelector(".foods");
+const foods = document.querySelector(".afood");
 const crt = document.querySelector(".crtbtn");
-foods.forEach((food) => {
-  const minusBtn = food.querySelector(".minus");
-  const plusBtn = food.querySelector(".plus");
+const menuData = [
+  {
+    id: 1,
+    foodimage: "images/PoundedYam.jpg",
+    title: "Pounded Yam And Efo",
+    price: 2000,
+  },
+  {
+    id: 2,
+    foodimage: "images/FriedRice.jpg",
+    title: "Fried Rice",
+    price: 1500,
+  },
+  {
+    id: 3,
+    foodimage: "images/JollofRice.jpg",
+    title: "Jollof Rice",
+    price: 1800,
+  },
+  {
+    id: 4,
+    foodimage: "images/Amala.jpg",
+    title: "Amala and Ewedu",
+    price: 1800,
+  },
+  {
+    id: 5,
+    foodimage: "images/Semo.jpg",
+    title: "Semo and Efo",
+    price: 1800,
+  },
+];
+card.innerHTML = menuData
+  .map(
+    (item) => `
+        <div class="afood">
+          <img src="${item.foodimage}" />
+            <div class="info">${item.title}
+              <br />
+              <div class="priceRow">
+                  <span class="price">
+                    ₦${item.price}
+                  </span>
+                   <div class="qtybtn">
+                      <button class="minus">-</button>
+                      <div class="quantity">0</div>
+                      <button class="plus">+</button>
+                   </div>
+              </div>
+              <div><button class="crtbtn">Add To Cart</button></div>
+            </div>
+        </div>
+      `
+  )
+  .join("");
+document.querySelectorAll(".afood").forEach((food) => {
   const qtyDiv = food.querySelector(".quantity");
-  let qty = parseInt(qtyDiv.textContent);
+  const plusBtn = food.querySelector(".plus");
+  const minusBtn = food.querySelector(".minus");
+  const price = food.querySelector(".price");
+  const usedprice = Number(price.textContent.replace("₦", ""));
+  let qty = 0;
   plusBtn.addEventListener("click", () => {
     qty++;
     qtyDiv.textContent = qty;
+    price.textContent = `₦${usedprice * qty}`;
   });
   minusBtn.addEventListener("click", () => {
-    if (qty > 0) qty--;
-    qtyDiv.textContent = qty;
-  });
-  crt.addEventListener("click", () => {
-    qty++;
-    qtyDiv.textContent = qty;
+    if (qty > 0) {
+      qty--;
+      qtyDiv.textContent = qty;
+      if (qty != 0) price.textContent = `₦${usedprice * qty}`;
+    }
   });
 });
